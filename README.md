34e1fa2dd379a925ba7f2f5d625802dab0c492ce
rkp678 <rkpierce678@gmail.com>
2022-02-04 21:31:57 -0600

    Pushing draft homework

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # Unit 19 Homework:  Crypto Wallet - Rachel Pierce
-## January 2022
+## February 2022
 
 ## *Instructions:*
 ### Three main tasks:
-1. 
+1. Import Ethereum Transaction Functions into the Fintech Finder Application
+2. Sign and Execute a Payment Transaction
+3. Inspect the Transaction on Ganache
 
 #
 ## *Please refer to the following documents in GitHub:*
-- The **XXX.py** file (open in Spyder) for code details and the **.png** and **.png** files in the images folder.
+- The **crypto_wallet.py** and **fintech_finder.py** files (open in Spyder) for code details and the **screenshots** files in the images folder.
 
 #
+## *Steps:*
+### Step 1: Import Ethereum Transaction Functions - Completed  
 
-## Step 1: 
+### Step 2: Sign and Execute a Payment Transaction - Completed
 
-
-## Step 2: 
-
-
-## Step 3: 
+### Step 3: Inspect - Completed  
+  
+    
+![image](./images/screenshot_streamlit.png)   
+![image](./images/screenshot_streamlit2.png)  
 
 
 
--- fintech_finder.py
+++ fintech_finder.py
@@ -81,6 +81,61 @@ w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 #  and send_transaction
 # YOUR CODE HERE
 
+def generate_account():
+    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
+    # Fetch mnemonic from environment variable.
+    mnemonic = os.getenv("MNEMONIC")
+
+    # Create Wallet Object
+    wallet = Wallet(mnemonic)
+
+    # Derive Ethereum Private Key
+    private, public = wallet.derive_account("eth")
+
+    # Convert private key into an Ethereum account
+    account = Account.privateKeyToAccount(private)
+
+    return account
+
+def get_balance(w3, address):
+    """Using an Ethereum account address access the balance of Ether"""
+    # Get balance of address in Wei
+    wei_balance = w3.eth.get_balance(address)
+
+    # Convert Wei value to ether
+    ether = w3.fromWei(wei_balance, "ether")
+
+    # Return the value in ether
+    return ether
+
+
+def send_transaction(w3, account, to, wage):
+    """Send an authorized transaction to the Ganache blockchain."""
+    # Set gas price strategy
+    w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
+
+    # Convert eth amount to Wei
+    value = w3.toWei(wage, "ether")
+
+    # Calculate gas estimate
+    gasEstimate = w3.eth.estimateGas({"to": to, "from": account.address, "value": value})
+
+    # Construct a raw transaction
+    raw_tx = {
+        "to": to,
+        "from": account.address,
+        "value": value,
+        "gas": gasEstimate,
+        "gasPrice": 0,
+        "nonce": w3.eth.getTransactionCount(account.address)
+    }
+
+    # Sign the raw transaction with ethereum account
+    signed_tx = account.signTransaction(raw_tx)
+
+    # Send the signed transactions
+    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
 ################################################################################
 # Fintech Finder Candidate Information
 
@@ -130,7 +185,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-# YOUR CODE HERE
+account = generate_account()
 
 ##########################################
 
@@ -147,7 +202,8 @@ st.sidebar.write(account.address)
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
 # YOUR CODE HERE
-
+ether_balance = get_balance(w3, account.address)
+st.sidebar.write(ether_balance)
 ##########################################
 
 # Create a select box to chose a FinTech Hire candidate
@@ -238,11 +294,12 @@ st.sidebar.markdown("## Total Wage in Ether")
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
 # YOUR CODE HERE
+wage = hourly_rate*hours
 
 # @TODO
 # Write the `wage` calculation to the Streamlit sidebar
 # YOUR CODE HERE
-
+st.sidebar.write(wage)
 ##########################################
 # Step 2 - Part 2:
 # * Call the `send_transaction` function and pass it three parameters:
@@ -269,7 +326,7 @@ if st.sidebar.button("Send Transaction"):
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
     # YOUR CODE HERE
-
+    transaction_hash = send_transaction(w3, account, candidate_address, wage)
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
 
